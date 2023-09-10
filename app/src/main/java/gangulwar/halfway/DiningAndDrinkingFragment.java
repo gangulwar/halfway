8dae55790b79139b0137b8b046d727106b05ddf0
gangulwar <aarshgangulwar23@gmail.com>
2023-09-10 23:39:51 +0530

    add fragment for sub category

---
+++ app/src/main/java/gangulwar/halfway/DiningAndDrinkingFragment.java
@@ -0,0 +1,27 @@
+package gangulwar.halfway;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DiningAndDrinkingFragment extends Fragment {
+
+
+    public DiningAndDrinkingFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_dining_and_drinking, container, false);
+
+        return view;
+    }
+}
